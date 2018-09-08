07b8bcc3fc62b4f870f6bd2f1306c7481b84e51c
Rikki Gibson <rikkigibson@gmail.com>
2018-09-08 10:46:26 -0700

    Move to TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-const express = require('express');
+import express = require('express');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
 	res.status(200);
-	res.end('<html><body>This is my <b>node.js</b> app!</html></body>');
+	res.end('<html><body>This is my <b>node.js</b> <i>TypeScript</i> app!</html></body>');
 });
 
 app.listen(port, function() {
